edb7ca6be4d9d626a789e37504abde1265b19bda
Yassir Ouali <yassir.ouali@thewallgames.com>
2017-02-07 12:42:58 +0000

    ajout du controller du Customer avec la gestion des points

---
+++ src/main/java/ma/ensao/egroupe/model/Customer.java
@@ -0,0 +1,41 @@
+package ma.ensao.egroupe.model;
+
+public class Customer {
+	private int id;
+	private int credit;
+	private String nom;
+	private int points;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCredit() {
+		return credit;
+	}
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
